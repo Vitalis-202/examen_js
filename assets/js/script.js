78b8f32daba687454a4327b203f931fd4153a746
VitaDev <pablo.perez.lee@gmail.com>
2025-01-19 02:25:31 -0300

    Agrega funcionalidad de grafico

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const input = document.querySelector('#input')
 const select = document.querySelector('#select')
 const btn_buscar = document.querySelector('#btn_buscar')
 const result = document.querySelector('#result')
+const canvasContainer = document.querySelector('.canvas-container')
+let chartInstance;
 
 //Buttton
 btn_buscar.addEventListener('click',async ()=>{
@@ -17,6 +19,8 @@ btn_buscar.addEventListener('click',async ()=>{
     result.innerHTML = `
     Resultado:$ ${resultado.toFixed(3)}
     `
+    renderizarGrafico(select.value)
+    canvasContainer.style.display = 'block'
 })
 
 //Funcion conectar a la API
@@ -36,39 +40,47 @@ const renderSelect = async () => {
     `
 }
 
-//Función de renderizar
-const renderDom = (data) => {
-    body.innerHTML = ''
-    data.items.array.forEach(personaje => {
-        body.innerHTML += `
-        <div class="card">
-            <img src="${personaje.image}" width=200px />
-            <h1>${personaje.name}</h1>
-            <p>${personaje.description}</p>
-        </div>
-        `
-    });
-}
-
-//Funcion API
-const main = async ()=> {
-    try {
-        const api = await getApi('https://mindicador.cl/api/')
-        console.log(api)
-        renderDom(api)
-    } catch (error) {
-        console.log(error.message)
+//Función renderizar grafico async
+const renderizarGrafico = async (moneda) =>{
+    const data = await getApi(`https://mindicador.cl/api/${moneda}`)
+    console.log(JSON.stringify(data,null,4))
+    //Extraemos las 10 ultimas fechas del historial y las invertimos
+    const datosInvertidos = data.serie.slice(0,10).reverse()
+    const labels = datosInvertidos.map(item=>item.fecha.slice(0,10))
+    const valores = datosInvertidos.map(item=>item.valor)
+    console.log(labels)
+    console.log(valores)
+    //Grafico
+    if(chartInstance){
+        chartInstance.destroy()
     }
+    const ctx = document.getElementById("grafico").getContext("2d")
+    chartInstance = new Chart(ctx, {
+        type:"line",
+        data:{
+            labels: labels, //Etiqueta eje x (fechas)
+            datasets:[{
+                label:`Historial ultimos 10 días(${moneda})`,//titulo
+                data:valores,//Datos del eje y
+                borderColor:"rgb(75,192,192)", //color de la línea del grafico
+                tension: 0.1, //Suaviza la linea del grafico
+            }]//
+        },
+        options:{
+            responsive: true,
+            maintainAspectRatio: true,
+            scales:{
+                x:{
+                    ticks:{
+                        maxRotation: 45,
+                        minRotation:0
+                    }
+                }
+            }
+        }
+    })
+    
 }
 
 //Llamado de funciones
 renderSelect()
-
-//Llamando funciones
-//main()
-//renderSelect()
-
-//Funciones del desafio
-//FormatearFechar
-//Actuarlizar grafico async (valor,tipomoneda)
-//btn addeventlistener
